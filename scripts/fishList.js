40073e21c1a9755f47cc11247aa1e573f4c132b9
Kristofer Cagle <142164483+KrisCagle@users.noreply.github.com>
2026-02-23 11:22:16 -0600

    Add functions for filtering fish by length

--- scripts/fishList.js
+++ scripts/fishList.js
@@ -18,3 +18,54 @@ export const fishList = () => {
 
   return htmlString
 };
+export const mostHolyFish = () => {
+  let holyFish = ""
+  for (const fish of database.fish) {
+    if (fish.length % 3 === 0) {
+      holyFish += `
+      <article class="fish">
+        <img src="${fish.image}" alt="${fish.name} the ${fish.species}" />
+        <h3>${fish.name}</h3>
+        <p><strong>Species:</strong> ${fish.species}</p>
+        <p><strong>Length:</strong> ${fish.length}</p>
+        <p><strong>Location:</strong> ${fish.location}</p>
+      </article>
+    `
+    }
+  }
+  return holyFish
+};
+export const soldierFish = () => {
+  let soldiers = ""
+  for (const fish of database.fish) {
+    if (fish.length % 5 === 0) {
+      soldiers += `
+      <article class="fish">
+        <img src="${fish.image}" alt="${fish.name} the ${fish.species}" />
+        <h3>${fish.name}</h3>
+        <p><strong>Species:</strong> ${fish.species}</p>
+        <p><strong>Length:</strong> ${fish.length}</p>
+        <p><strong>Location:</strong> ${fish.location}</p>
+      </article>
+    `
+    }
+  }
+  return soldiers
+}
+
+export const regularFish = () => {
+  let regulars = ""
+  for (const fish of database.fish) {
+    if (fish.length % 3 !== 0 && fish.length % 5 !== 0) {
+      regulars += `
+      <article class="fish">
+        <img src="${fish.image}" alt="${fish.name} the ${fish.species}" />
+        <h3>${fish.name}</h3>
+        <p><strong>Species:</strong> ${fish.species}</p>
+        <p><strong>Length:</strong> ${fish.length}</p>
+        <p><strong>Location:</strong> ${fish.location}</p></article>
+    `
+    }
+  }
+  return regulars
+}
