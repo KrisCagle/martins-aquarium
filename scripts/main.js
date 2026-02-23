83d569763d5eaf8b87133b55909ad4db30451fba
Kristofer Cagle <142164483+KrisCagle@users.noreply.github.com>
2026-02-23 11:20:31 -0600

    Add additional fish types to HTML rendering

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-import { fishList } from "./fishList.js"
+import { fishList, mostHolyFish, soldierFish, regularFish } from "./fishList.js"
 import { tipList } from "./tipList.js"
 import { locationList } from "./locationList.js"
 
@@ -6,8 +6,11 @@ import { locationList } from "./locationList.js"
 const fishHTML = fishList()
 const tipHTML = tipList()
 const locationHTML = locationList()
+const holyFishHTML = mostHolyFish()
+const soldierFishHTML = soldierFish()
+const regularFishHTML = regularFish()
 
 // Render each one into the correct DOM element
-document.querySelector("#fishList").innerHTML = fishHTML
+document.querySelector("#fishList").innerHTML = fishHTML `${holyFishHTML}${soldierFishHTML}${regularFishHTML}`
 document.querySelector("#tipList").innerHTML = tipHTML
 document.querySelector("#locationList").innerHTML = locationHTML
